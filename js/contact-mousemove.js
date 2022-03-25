98b5f1070a126cba964743fa5e925ec592c1cca7
CarlLinusHedlund <carl.hedlund@stud.noroff.no>
2022-03-25 23:00:12 +0100

    update

--- js/contact-mousemove.js
+++ js/contact-mousemove.js
@@ -2,10 +2,8 @@ document.addEventListener("mousemove", parallax);
 function parallax(e){
     document.querySelectorAll(".green-blob").forEach(function(move){
         var movingValue = move.getAttribute("data-value");
-        var x = (e.clientX + movingValue) / 1400;
-        var y = (e.clientY + movingValue) / 1400;
-        console.log(movingValue)
-
+        var x = (e.clientX * movingValue) / 1400;
+        var y = (e.clientY * movingValue) / 1400;
         move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
     });
 } 
