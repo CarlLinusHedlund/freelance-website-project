23194eefdc8880159699e73829af6fb470c2c68c
CarlLinusHedlund <carl.hedlund@stud.noroff.no>
2022-03-27 18:41:16 +0200

    update

---
+++ js/local-storage.js
@@ -0,0 +1,35 @@
+// const body = document.querySelector('body');
+// const darkModeToggle = document.querySelector('.dark-mode-button');
+// let darkMode = localStorage.getItem('darkMode');
+// let lang = localStorage.getItem('lang');
+
+// // disables darkmode, adds value to localStorage
+// const enableDarkMode = () => {
+//   body.classList.add('dark');
+//   localStorage.setItem('darkMode', 'enabled');
+//   localStorage.setItem('lang', 'en');
+// };
+
+// // disables darkmode, adds value to localStorage
+// const disableDarkMode = () => {
+//   body.classList.remove('dark');
+//   localStorage.setItem('darkMode', null);
+//   localStorage.setItem('lang', 'no');
+// };
+
+// // enabels darkmode on reload if toggled
+// if (darkMode === 'enabled') {
+//   enableDarkMode();
+// }
+
+// // toggles darkmode
+// darkModeToggle.onclick = function toggleDarkMode() {
+//   darkMode = localStorage.getItem('darkMode');
+//   if (darkMode !== 'enabled') {
+//     enableDarkMode();
+//   } else {
+//     disableDarkMode();
+//   }
+// };
+
+const 
